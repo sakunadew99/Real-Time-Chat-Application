c3abbba95c9c2fe60125390849e722061c5648fe
Sakuna Dewanarayana <68386969+sakunadew99@users.noreply.github.com>
2024-06-02 22:02:50 +0530

    update origin

--- chat-application/backend/server.js
+++ chat-application/backend/server.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://real-time-chat-application-frontend-six.vercel.app/",
+    origin: "https://real-time-chat-application-frontend-six.vercel.app",
     methods: ["GET", "POST"],
   },
 });
