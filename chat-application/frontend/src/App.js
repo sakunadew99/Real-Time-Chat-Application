9f1c7cfcc765e84cd56e6eed4c02f8b68fa3976d
Sakuna Dewanarayana <68386969+sakunadew99@users.noreply.github.com>
2024-06-02 22:51:04 +0530

    update App.js

--- chat-application/frontend/src/App.js
+++ chat-application/frontend/src/App.js
@@ -1,41 +1,23 @@
 import "./App.css";
 import io from "socket.io-client";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Chat from "./Chat";
 
-const socket = io.connect("https://real-time-chat-application-backend-eight.vercel.app", {
-});
+const socket = io.connect("https://real-time-chat-application-backend-eight.vercel.app");
 
 function App() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
   const [showChat, setShowChat] = useState(false);
 
-  useEffect(() => {
-    // Handling socket connection errors
-    socket.on('connect_error', (err) => {
-      console.error('Connection error:', err);
-      alert('Failed to connect to the server. Please try again later.');
-    });
-
-    // Clean up on component unmount
-    return () => {
-      socket.off('connect_error');
-      socket.disconnect();
-    };
-  }, []);
-
   const joinRoom = () => {
     if (username !== "" && room !== "") {
       socket.emit("join_room", { username, room });
       setShowChat(true);
-    } else {
-      alert('Please enter both username and room ID');
     }
   };
 
   const handleLogout = () => {
-    socket.emit('leave_room', { username, room }); // Optionally emit leave room event
     socket.disconnect();
     setShowChat(false);
     setUsername("");
@@ -51,12 +33,17 @@ function App() {
           <input
             type="text"
             placeholder="User Name..."
-            onChange={(event) => setUsername(event.target.value)}
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
           />
           <input
             type="text"
             placeholder="Room ID..."
-            onChange={(event) => setRoom(event.target.value)}
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+            
           />
           <button onClick={joinRoom}>Join A Room</button>
         </div>
